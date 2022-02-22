501ff6021eb332bd8c992a7248ef6cacae166b03
TeoPKI <98078358+TeoPKI@users.noreply.github.com>
2022-02-22 10:51:30 +0200

    Add files via upload
    yhdyssanat special case ja käyttöliittymää

--- tavuttaja.js
+++ tavuttaja.js
@@ -2,34 +2,52 @@ const konsonantit = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p',
 const diftongit = ['ei', 'öi', 'äi', 'oi', 'ai', 'yi', 'ui', 'iy', 'iu', 'ey', 'öy', 'äy', 'eu', 'ou', 'au', 'ie', 'yö', 'uo']
 
 
-function testi() {
-    let testiSanat = ['suomenhevonen', 'newforestinponi', 'lapinkeltaperhonen',
+// function testi() {
+//     let testiSanat = ['suomenhevonen', 'newforestinponi', 'lapinkeltaperhonen',
 
-        'lapinlehmä', 'ayrshirelehmä', 'ylämaankarja', 'kainuunlammas',
+//         'lapinlehmä', 'ayrshirelehmä', 'ylämaankarja', 'kainuunlammas',
 
-        'itämerennorppa', 'bengalintiikeri', 'aasiannorsu', 'kuubankrokotiili',
+//         'itämerennorppa', 'bengalintiikeri', 'aasiannorsu', 'kuubankrokotiili',
 
-        'tasmaniankaija', 'meksikonkaklattaja', 'panamansmaragdikolibri', 'pohjoisenkalliotöyhtöpingviini',
+//         'tasmaniankaija', 'meksikonkaklattaja', 'panamansmaragdikolibri', 'pohjoisenkalliotöyhtöpingviini',
 
-        'madagaskarinpäivägekko', 'puertoriconboa', 'kaliforniankuningaskäärme', 'australianpallohai',
+//         'madagaskarinpäivägekko', 'puertoriconboa', 'kaliforniankuningaskäärme', 'australianpallohai',
 
-        'ruusuruohomaamehiläinen', 'töpökääpiötypäkkö', 'lapinlehmä', 'suomenpystykorva', 'Hakaniementori', 'sika-nautajauheliha', 'lentokonesuihkuturbiinimoottoriapumekaanikkoaliupseerioppilas', 'aasia', 'Idiootti', 'Booli', 'Rakkaus', 'Majakka', 'Kirsikka', 'Nauraa', 'Gladiaattori', 'Pallonpuoliskon', 'Espanja', 'Kriittinen', 'asia', 'raapia', 'Sementti', 'Banaani', 'Ohjelmistot', 'Kausi', 'Tehdas', 'Persilja', 'Vastaava', 'Periskooppi', 'Vastaanottaa', 'Klaustrofobia', 'Meteoriitti'];
+//         'ruusuruohomaamehiläinen', 'töpökääpiötypäkkö', 'lapinlehmä', 'suomenpystykorva', 'Hakaniementori', 'sika-nautajauheliha', 'lentokonesuihkuturbiinimoottoriapumekaanikkoaliupseerioppilas', 'aasia', 'Idiootti', 'Booli', 'Rakkaus', 'Majakka', 'Kirsikka', 'Nauraa', 'Gladiaattori', 'Pallonpuoliskon', 'Espanja', 'Kriittinen', 'asia', 'raapia', 'Sementti', 'Banaani', 'Ohjelmistot', 'Kausi', 'Tehdas', 'Persilja', 'Vastaava', 'Periskooppi', 'Vastaanottaa', 'Klaustrofobia', 'Meteoriitti'];
 
-    for (let i = 0; i < testiSanat.length; i++) {
-        tavuta(testiSanat[i]);
-    }
-}
+//     for (let i = 0; i < testiSanat.length; i++) {
+//         tavuta(testiSanat[i]);
+//     }
+// }
 
-function tavutaInput(input) {
+// tavuttaa syötteen erotellen yhdyssanat eri sanoiksi halutessaan.
+function tavutaInput() {
     let display = document.getElementById('display');
 
-    let regex = /^[a-zA-Z0-9]+$/;
-    
-    
+    let input = document.getElementById('input-box').value;
+    let yhdyssana = input.split('-');
+    let output = "";
 
-    display.innerHTML = tavuta(document.getElementById('input-box').value);
+    // jaa merkatut yhdyssanat eri sanoiksi
+    for (let i = 0; i < yhdyssana.length; i++) {
+        console.log(yhdyssana[i]);
+        output += tavuta(yhdyssana[i]);
+
+        // tavu yhdyssanojen väliin
+        if (yhdyssana.length > 1 && i < yhdyssana.length - 1) {
+            output += "-";
+        }
+    }
+
+    let outputCapitalized = output.toLocaleLowerCase();
+    let capital = outputCapitalized.charAt(0).toUpperCase();
+    outputCapitalized = outputCapitalized.slice(1);
+    outputCapitalized = capital + outputCapitalized;
+
+    display.innerHTML = outputCapitalized;
 }
 
+// tavuttaa erillisanan
 function tavuta(sana) {
     let output = "";
 
@@ -43,9 +61,7 @@ function tavuta(sana) {
         konsonantit[i] = konsonantit[i].normalize();
     }
 
-
     // konsonantti => vokaali = tavu
-
     function konsonanttiSaanto(index) {
 
         // TODO: toimii mutta onko oikein? tavuttaa esim 'aliupseeri' oikein.
@@ -74,7 +90,6 @@ function tavuta(sana) {
 
     // vokaali => vokaali = tavu
     function vokaaliSaanto(index) {
-
         if (!konsonantit.includes(sana[[index]]) && !konsonantit.includes(sana[index + 1])) {
             // ei ole pitkävokaali
             if (sana[index] !== sana[index + 1]) {
@@ -94,6 +109,9 @@ function tavuta(sana) {
         return sana[index] === '-' || sana[index + 1] === '-';
     }
 
+    if (sana.length <= 2)
+        return sana;
+
     for (let i = 0; i < sana.length; i++) {
         output += sana[i];
 
@@ -105,8 +123,5 @@ function tavuta(sana) {
         }
     }
 
-    let outputCapitalized = output.charAt(0).toUpperCase() + output.slice(1);
-
-    console.log(outputCapitalized);
-    return outputCapitalized;
+    return output;
 }
